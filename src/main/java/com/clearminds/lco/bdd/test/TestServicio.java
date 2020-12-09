c4f185885cccd9d6ae4afb32ab845001126d244a
xaviz5 <xavier5-20_lenin@hotmail.com>
2020-12-09 11:37:06 -0500

    Creacion de metodo insertarEstudiante en ServicioEstudiante y se creo la clase TestServicio, con la evidencia

---
+++ src/main/java/com/clearminds/lco/bdd/test/TestServicio.java
@@ -0,0 +1,18 @@
+package com.clearminds.lco.bdd.test;
+
+import com.clearminds.lco.dtos.Estudiante;
+import com.clearminds.lco.excepciones.BDDException;
+import com.clearminds.lco.servicios.ServicioEstudiante;
+
+public class TestServicio {
+
+	public static void main(String[] args) {
+		ServicioEstudiante srvEstudiante = new ServicioEstudiante();
+		try {
+			srvEstudiante.insertarEstudiante(new Estudiante("Juan", "Perez"));
+		} catch (BDDException e) {
+			e.printStackTrace();
+			e.getMessage();
+		}
+	}
+}
--- src/main/java/com/clearminds/lco/servicios/ServicioEstudiante.java
+++ src/main/java/com/clearminds/lco/servicios/ServicioEstudiante.java
@@ -1,7 +1,14 @@
 package com.clearminds.lco.servicios;
 
+import com.clearminds.lco.dtos.Estudiante;
+import com.clearminds.lco.excepciones.BDDException;
+
 public class ServicioEstudiante extends ServicioBase{
 
-	
+	public void insertarEstudiante(Estudiante estudiante) throws BDDException{
+		abrirConexion();
+		System.out.println("Insertando estudiante: "+estudiante);
+		cerrarConexion();
+	}
 	
 }
